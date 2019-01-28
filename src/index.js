acaf796dbda35066cd471de170367ebd3b7e2afc
摇太阳 <yaotaiyang99@gmail.com>
2019-01-28 20:54:01 +0800

    add zoom && drag stage

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { Link } from './link'
 import * as spritejs from 'spritejs'
 import { _steps, _links, _workflow, _render } from './symbolNames'
 import * as functions from './functions'
-const { Scene, Layer } = spritejs;
+import { timingSafeEqual } from 'crypto';
+const { Scene, Layer, Group } = spritejs;
 
 spritejs.use(install);
 class Workflow extends Base {
@@ -15,10 +16,14 @@ class Workflow extends Base {
     /*****
   * selector:css选择器
   * size:canvas大小
+  * zoom:是否搜房
+  * zoomMax:最大缩放
+  * zoomMin:最小缩放
   * **/
     this.attr(Object.assign({
       'selector': '',
-      'size': [ 600, 400 ]
+      'size': [ 600, 400 ],
+      'zoom': [ 0.5, 2 ]
     }, attrs));
     const { selector, size } = this.attr();
     const scene = new Scene(selector, {
@@ -27,12 +32,17 @@ class Workflow extends Base {
     })
     this[ _steps ] = [];
     this[ _links ] = [];
-    this.stage = scene.layer();
+    let layer = scene.layer();
+    scene.delegateEvent('mousewheel', document); //sprite 元素侦听mousewheel事件
+    layer.append(this.container);
+    if (this.attr('zoom') !== false) {
+      zoom.call(this, layer, this.container);
+    }
     delete this.renderBox;
     delete this.sizeBox;
   }
   /**
-   * 
+   * workflow添加子对象 step link
    * @param {sprite} ['Step' 'Link']
    */
   append(sprite) {
@@ -43,8 +53,54 @@ class Workflow extends Base {
     } else if (sprite instanceof Link) {
       this[ _links ].push(sprite);
     }
-    this.stage.append(sprite[ _render ]());
+    this.container.append(sprite[ _render ]());
     sprite.dispatchEvent('mounted', {});
   }
+}
+function zoom(layer, group) {
+  let oX, oY;
+  let startX, startY;
+  let draged = false;
+  layer.on('mousedown', (e) => {
+    let $target = e.target;
+    if ($target === layer || $target === group) {
+      oX = e.offsetX;
+      oY = e.offsetY;
+      [ startX, startY ] = group.attr('pos');
+      draged = true;
+    }
+  });
+  layer.on('mousemove', (e) => {
+    let $target = e.target;
+    if (draged) {
+      let dx = e.offsetX - oX;
+      let dy = e.offsetY - oY;
+      group.transition(0.01).attr({ pos: [ startX + dx, startY + dy ] });
+    }
+  })
+  layer.on('mouseup', (e) => {
+    draged = false;
+  });
+  layer.on('mousewheel', (e) => {
+    e.preventDefault();
+    const [ scaleX, scaleY ] = group.attr('scale');
+    let [ w, h ] = group.attr("size");
+    let direction = 1;
+    if (e.originalEvent.wheelDelta < 0) {//向下滚动
+      direction = -1;
+    }
+    const dscale = 0.5 * direction;
+    /** 计算以鼠标点为中心缩放 **/
+    const [ oAnchorX, oAnchorY ] = group.attr('anchor');
+    let pX = (oAnchorX * w + e.offsetX) / w; //鼠标点相对占比
+    let pY = (oAnchorY * h + e.offsetY) / h;
+    const [ oX, oY ] = group.attr("pos");
+    let dx = w * dscale * pX;
+    let dy = h * dscale * pY;
+    const zoom = this.attr('zoom');
+    if (scaleX + dscale > zoom[ 0 ] && scaleX + dscale < zoom[ 1 ]) {
+      group.transition(0.1).attr({ scale: [ scaleX + dscale, scaleX + dscale ], pos: [ oX - dx, oY - dy ] });
+    }
+  });
 }
 export { Workflow, Link, Step, functions }
